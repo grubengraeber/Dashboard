ef64b6ae68ecc4551f7464c05bf7a326548b5d3a
patrickp2502 <patrick.pradka@gmail.com>
2022-07-27 19:20:01 +0200

    Working DTO Transfer on /api/shopping-list/{id} transfered DTOConversion to Communication Layer

--- dashboard/src/main/java/at/enough/dashboard/shoppinglist/controller/ShoppingListController.java
+++ dashboard/src/main/java/at/enough/dashboard/shoppinglist/controller/ShoppingListController.java
@@ -1,8 +1,10 @@
 package at.enough.dashboard.shoppinglist.controller;
 
 
-import at.enough.dashboard.shoppinglist.dto.ShoppingListDTO;
+import at.enough.dashboard.shoppinglist.controller.dto.ShoppingListDTO;
+import at.enough.dashboard.shoppinglist.controller.dto.ShoppingListDTOConverter;
 import at.enough.dashboard.shoppinglist.logic.ShoppingListService;
+import at.enough.dashboard.shoppinglist.model.ShoppingList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,27 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-//TODO ask El where to put DTO converters? in controller or otherhere?
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/shopping-list")
 public class ShoppingListController {
     private final ShoppingListService shoppingListService;
+    private final ShoppingListDTOConverter shoppingListDTOConverter;
 
     @GetMapping
     public List<ShoppingListDTO> getShoppingLists() {
-        return shoppingListService.getAll();
+        return shoppingListService.getAll().stream()
+                .map(shoppingListDTOConverter::toDTO)
+                .collect(Collectors.toList());
 
     }
 
-    //Todo errormanagement?
     @GetMapping("{id}")
-    public ShoppingListDTO getById(@PathVariable long id) {
+    public ShoppingList getById(@PathVariable long id) {
 
-        Optional<ShoppingListDTO> shoppingListDTOOptional = shoppingListService.findBy(id);
+        Optional<ShoppingList> shoppingListDTOOptional = shoppingListService.findBy(id);
         if (shoppingListDTOOptional.isEmpty()) {
-            System.out.println("works");
             return null;
 
         }
--- dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ShoppingListService.java
+++ dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ShoppingListService.java
@@ -1,41 +1,29 @@
 package at.enough.dashboard.shoppinglist.logic;
 
-import at.enough.dashboard.shoppinglist.dto.ShoppingListDTO;
-import at.enough.dashboard.shoppinglist.dto.ShoppingListDTOConverter;
 import at.enough.dashboard.shoppinglist.model.ShoppingList;
 import at.enough.dashboard.shoppinglist.model.repository.ShoppingListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ShoppingListService {
     private final ShoppingListRepository shoppingListRepository;
-    private final ShoppingListDTOConverter shoppingListDTOConverter;
 
-    public List<ShoppingListDTO> getAll() {
-        List<ShoppingList> shoppingLists = shoppingListRepository.findAll();
-        if (shoppingLists.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        return shoppingLists.stream()
-                .map(shoppingListDTOConverter::toDTO)
-                .collect(Collectors.toList());
+    public List<ShoppingList> getAll() {
+        return shoppingListRepository.findAll();
     }
 
-    public Optional<ShoppingListDTO> findBy(long id) {
+    public Optional<ShoppingList> findBy(long id) {
         Optional<ShoppingList> shoppingListOptional = shoppingListRepository.findBy(id);
         if (shoppingListOptional.isEmpty()) {
             return Optional.empty();
         }
 
-        return Optional.of(shoppingListDTOConverter.toDTO(shoppingListOptional.get()));
+        return shoppingListOptional;
 
 
     }
