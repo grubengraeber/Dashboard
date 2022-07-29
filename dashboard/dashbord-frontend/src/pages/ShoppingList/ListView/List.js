dcc5e0e6ab9bd9cbc4aa1a3214a4a1f32f75c1e9
Fabio Tietz <fabiotietz0@gmail.com>
2022-07-29 17:47:22 +0200

    Cleaning up (getting rid of console warnings)

--- dashboard/dashbord-frontend/src/pages/ShoppingList/ListView/List.js
+++ dashboard/dashbord-frontend/src/pages/ShoppingList/ListView/List.js
@@ -1,36 +1,18 @@
 import {ListGroup} from "react-bootstrap";
 import {ListItem} from "./ListItem";
 
-export const List = props => {
-    const ListItems = [
-        {
-            "id": 1,
-            "name": "distance test1",
-            "count": 11,
-            "done": false
-        },
-        {
-            "id": 2,
-            "name": "distance test2",
-            "count": 20,
-            "done": false
-        },
-        {
-            "id": 3,
-            "name": "distance test3",
-            "count": 31,
-            "done": false
-        }
-    ]
+export const List = ({list}) => {
+
     return (
         <>
             <ListGroup>
-                {ListItems.map(item =>
+                {list.map(item =>
                     item.done ? "" :
                         <ListItem id={item.id}
                                   checked={item.done}
                                   count={item.count}
                                   name={item.name}
+                                  key={item.id}
                         />
                 )}
 
--- dashboard/dashbord-frontend/src/pages/ShoppingList/ListView/ListContainer.js
+++ dashboard/dashbord-frontend/src/pages/ShoppingList/ListView/ListContainer.js
@@ -4,12 +4,31 @@ import {BorderManagement} from "../../PageStyle/BorderManagement";
 import {NewItemModal} from "./NewItemModal";
 
 export const ListContainer = props => {
-
+    let list = [
+        {
+            "id": 1,
+            "name": "Banane(n)",
+            "count": 11,
+            "done": false
+        },
+        {
+            "id": 2,
+            "name": "Smoothie(s)",
+            "count": 8,
+            "done": false
+        },
+        {
+            "id": 3,
+            "name": "Zwiebel(n)",
+            "count": 5,
+            "done": false
+        }
+    ]
     return (
         <>
             <Card style={BorderManagement}>
                 <NewItemModal/>
-                <List/>
+                <List list={list}/>
             </Card>
         </>
     );
