b22bb9bcd6c1c387c27474cf6a352a85bce5e932
Fabio Tietz <fabiotietz0@gmail.com>
2022-08-11 10:27:24 +0200

    Merged into main

--- dashboard/dashbord-frontend/src/pages/ShoppingList/ShoppingList.js
+++ dashboard/dashbord-frontend/src/pages/ShoppingList/ShoppingList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import React from "react";
+import BasicShoppingListItem from "./itemTemplate/BasicShoppingListItem"
+
+const shoppingListItems = [];
 
 export const ShoppingList = props => {
     return (
         <>
             Shoppinglist
+            {shoppingListItems.map(item => {
+                return (
+                    <BasicShoppingListItem />
+                )
+
+            })}
         </>
     );
 };
