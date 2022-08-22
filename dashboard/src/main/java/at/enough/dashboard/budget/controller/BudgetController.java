50a4340a16531e68cc09fc120da37448fb180d79
patrickp2502 <patrick.pradka@gmail.com>
2022-08-22 11:31:59 +0200

    added RequestMapping for Entries put/post added edit and add methods to BudgetService

--- dashboard/src/main/java/at/enough/dashboard/budget/controller/BudgetController.java
+++ dashboard/src/main/java/at/enough/dashboard/budget/controller/BudgetController.java
@@ -3,9 +3,7 @@ package at.enough.dashboard.budget.controller;
 import at.enough.dashboard.budget.persistence.model.Expense;
 import at.enough.dashboard.budget.service.BudgetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -29,5 +27,19 @@ public class BudgetController {
     public Map<String, Double> getChartData() {
         return budgetService.getChartData();
     }
+
+
+    @PostMapping("/expenses")
+    public Expense add(@RequestBody Expense expense) {
+        return budgetService.add(expense);
+    }
+
+    //todo add filter/interceptor for state check
+    @PutMapping("/expenses/{expenseId}")
+    public void edit(@PathVariable long expenseId, @RequestBody Expense expense) {
+        budgetService.edit(expenseId, expense);
+
+    }
+
 
 }
--- dashboard/src/main/java/at/enough/dashboard/budget/service/BudgetService.java
+++ dashboard/src/main/java/at/enough/dashboard/budget/service/BudgetService.java
@@ -5,6 +5,7 @@ import at.enough.dashboard.budget.persistence.repository.ExpenseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,12 +23,28 @@ public class BudgetService {
 
     }
 
+    public Expense findExpenseById(long id) {
+        return expenseRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("There is no Expense with ID " + id));
+    }
+
 
     public Map<String, Double> getChartData() {
         return getAll().stream()
                 .collect(Collectors.groupingBy(element -> element.getCategory().getName(),
                         Collectors.summingDouble(Expense::getCost)));
 
+
+    }
+
+    public Expense add(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public void edit(long expenseId, Expense expense) {
+        Expense persistedExpense = findExpenseById(expenseId);
+        persistedExpense.setId(expenseId);
+        expenseRepository.save(persistedExpense);
 
     }
 }
