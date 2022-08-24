b2bdbc2554f4facacc5d080ed775e7a01d7166b7
Fabio Tietz <fabiotietz0@gmail.com>
2022-08-24 22:46:41 +0200

    Adding shopping list entries work in the backend now as well

--- dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ShoppingListEntryService.java
+++ dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ShoppingListEntryService.java
@@ -1,11 +1,18 @@
 package at.enough.dashboard.shoppinglist.logic;
 
+import at.enough.dashboard.shoppinglist.dao.repository.ItemRepository;
 import at.enough.dashboard.shoppinglist.dao.repository.ShoppingListEntryRepository;
+import at.enough.dashboard.shoppinglist.dao.repository.ShoppingListRepository;
+import at.enough.dashboard.shoppinglist.model.AddItem;
+import at.enough.dashboard.shoppinglist.model.Item;
+import at.enough.dashboard.shoppinglist.model.ShoppingList;
 import at.enough.dashboard.shoppinglist.model.ShoppingListEntry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -13,6 +20,8 @@ import java.util.Optional;
 public class ShoppingListEntryService {
 
     private final ShoppingListEntryRepository shoppingListItemRepository;
+    private final ShoppingListRepository shoppingListRepository;
+    private final ItemRepository itemRepository;
 
     public Optional<ShoppingListEntry> edit(long id, ShoppingListEntry entry) {
         Optional<ShoppingListEntry> persistedEntryOptional = shoppingListItemRepository.findById(id);
@@ -35,4 +44,25 @@ public class ShoppingListEntryService {
         shoppingListItemRepository.delete(shoppingListEntry);
     }
 
+    public void add(AddItem addItem, Long listId) {
+        Optional<ShoppingList> optionalShoppingList = shoppingListRepository.findById(listId);
+        if (optionalShoppingList.isEmpty()) {
+            return;
+        }
+        Item item;
+        if (itemRepository.existsByName(addItem.getName())) {
+            item = itemRepository.findFirstByName(addItem.getName());
+        } else {
+            item = Item.builder().name(addItem.getName()).build();
+            itemRepository.save(item);
+        }
+        ShoppingList shoppingList = optionalShoppingList.get();
+        ShoppingListEntry shoppingListEntry = ShoppingListEntry.builder()
+                .shoppingList(shoppingList)
+                .item(item)
+                .amount(addItem.getAmount())
+                .addedTime(LocalDateTime.now())
+                .build();
+        shoppingListItemRepository.save(shoppingListEntry);
+    }
 }
--- dashboard/src/main/java/at/enough/dashboard/shoppinglist/model/AddItem.java
+++ dashboard/src/main/java/at/enough/dashboard/shoppinglist/model/AddItem.java
@@ -1,2 +1,16 @@
-package at.enough.dashboard.shoppinglist.model;public class AddItem {
+package at.enough.dashboard.shoppinglist.model;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Component
+public class AddItem {
+    private String name;
+    private int amount;
 }
