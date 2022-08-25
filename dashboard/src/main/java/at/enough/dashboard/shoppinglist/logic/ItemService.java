9ce6bafcefc73bcf3e1fe58221e456bb2f8fbe25
Fabio Tietz <fabiotietz0@gmail.com>
2022-08-25 16:58:09 +0200

    Created item controller for frontend autocomplete implementation

--- dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ItemService.java
+++ dashboard/src/main/java/at/enough/dashboard/shoppinglist/logic/ItemService.java
@@ -1,2 +1,20 @@
-package at.enough.dashboard.shoppinglist.logic;public class ItemService {
+package at.enough.dashboard.shoppinglist.logic;
+
+import at.enough.dashboard.shoppinglist.dao.repository.ItemRepository;
+import at.enough.dashboard.shoppinglist.model.Item;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public List<Item> getAll() {
+        return itemRepository.findAll();
+    }
+
 }
