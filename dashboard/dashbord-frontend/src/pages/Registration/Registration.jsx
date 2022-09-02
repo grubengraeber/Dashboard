bab45aac95bd443da612e1115dea75bad7f4eea0
Fabio Tietz <fabiotietz0@gmail.com>
2022-09-02 12:32:24 +0200

    Validation before submitting is implemented

--- dashboard/dashbord-frontend/src/pages/Registration/Registration.jsx
+++ dashboard/dashbord-frontend/src/pages/Registration/Registration.jsx
@@ -5,7 +5,7 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import PersonIcon from '@mui/icons-material/Person';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import { yupResolver } from "@hookform/resolvers"
+import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup";
 
 
@@ -27,16 +27,31 @@ function Registration() {
         mouseDownEvent.preventDefault();
     }
 
-    // VALIDATION SCHEMA
+    // VALIDATION SCHEMA matches(REGEX, String: error message)
     const schema = yup.object().shape({
       firstName: yup
         .string()
         .matches(/^([^0-9]*)$/, "First name should not contain numbers.")
+        .matches(/^[a-zA-Z0-9_.-]*$/, "No special characters.")
         .required("First name is required."),
       lastName: yup
         .string()
         .matches(/^([^0-9]*)$/, "Last name should not contain numbers.")
-        .required("Last name is required.")
+        .matches(/^[a-zA-Z0-9_.-]*$/, "No special characters.")
+        .required("Last name is required."),
+        emailAddress: yup
+          .string()
+          .matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Enter a valid email address.")
+          .required("Email address is required."),
+        password: yup
+          .string()
+          .matches(/[^ ]{8}/, "Password should contain at least 8 characters.")
+          .matches(/(?=.*[a-z])/, "Password should contain at least one lower case letter.")
+          .matches(/(?=.*[A-Z])/, "Password should contain at least one upper case letter.")
+          .matches(/(?=.*\d)/, "Password should contain at least one digit.")
+          .matches(/(?=.*\W)/, "Password should contain at least one special character.")
+          .required("Password is required.")
+
     })
 
     // NEXT LINE FOR FORM VALIDATION
@@ -71,8 +86,8 @@ function Registration() {
                     required 
                     type={"text"}
                     error={!!errors.firstName}
-                    helperText={ok}
-                    ref={register}
+                    helperText={errors && errors.firstName && errors.firstName.message ? errors.firstName.message : null}
+                    {...register("firstName", {required: "Required"})}
                     InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -94,6 +109,8 @@ function Registration() {
                         )
                       }}
                     {...register("lastName", {required: "Required"})}
+                    error={!!errors.lastName}
+                    helperText={errors && errors.lastName && errors.lastName.message ? errors.lastName.message : null}
                     />
                 </Stack>
                 <TextField 
@@ -110,6 +127,8 @@ function Registration() {
                   }}
                 autoComplete={"email"}
                 {...register("emailAddress", {required: "Required"})}
+                error={!!errors.emailAddress}
+                helperText={errors && errors.emailAddress && errors.emailAddress.message ? errors.emailAddress.message : null}
                 />
                 {/* <TextField label="Username" required type={"text"}/> */}
                 <TextField 
@@ -117,27 +136,32 @@ function Registration() {
                 required 
                 type={showPassword ? "text" : "password"}
                 InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={togglePassword}
-                        onMouseDown={onMouseDown}
-                        edge="end"
-                        >
-                            {showPassword ? <VisibilityIcon/> : <VisibilityOffIcon/> }
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={togglePassword}
+                      onMouseDown={onMouseDown}
+                      edge="end"
+                      >
+                          {showPassword ? <VisibilityIcon/> : <VisibilityOffIcon/> }
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                error={!!errors.password}
+                helperText={errors && errors.password && errors.password.message ? errors.password.message : null}
                 autoComplete={"new-password"}
                 {...register("password", {required: "Required"})}
                 />
-                <Controller as={Checkbox} control={control} name="remember" defaultValue={false}/>
-                <FormControlLabel 
-                control={<Checkbox  name="remember" />} 
-                label={"Remember me"}
-                />
+                <Stack direction={"row"} spacing={1}>
+                  <Controller as={Checkbox} control={control} name="remember" render={({field}) => (
+                    
+                    <Checkbox name="remember" {...field}/> 
+                    
+                  )} defaultValue={false}/> 
+                  <Typography display={"flex"} alignItems={"center"}>Remember me</Typography>
+                </Stack>
                 <Button 
                 variant="contained" 
                 type={'submit'}
